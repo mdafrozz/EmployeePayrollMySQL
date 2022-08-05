940776a426d702653e8eaecaaa75e32cb4a0637f
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-05 12:29:26 +0530

    Refactor: Update the salary i.e. the base pay for Employee sachin to 3000000.00 and sync it with Database

--- src/main/java/controller/EmployeePayroll.java
+++ src/main/java/controller/EmployeePayroll.java
@@ -7,35 +7,18 @@ import java.util.Scanner;
 
 public class EmployeePayroll {
     static Scanner sc;
+    PayrollDBService payrollDbService;
 
     public static void main(String[] args) {
         System.out.println("************** Welcome to Employee Payroll Service (MySQL) **************");
 
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-
-        boolean exit = false;
-        while (!exit) {
-            System.out.println("<---------------- Select Menu ---------------->");
-            System.out.println("1.Fetch Data\n0.Quit");
-            sc = new Scanner(System.in);
-            System.out.println("<-------------- Enter your option ------------->");
-            int option = sc.nextInt();
-
-            switch (option) {
-                case 1:
-                    employeePayrollService.getAllEmployeesData();
-                    break;
-                case 0:
-                    exit = true;
-                    System.out.println("******** Thank You **********");
-                    break;
-                default:
-                    System.out.println("Invalid option.Please try again.");
-            }
-
-        }
+        System.out.println("<---------------- Fetch Employee Data ---------------->");
+        employeePayrollService.getAllEmployeesData();
+        System.out.println("<---------------- Update Salary with 3000000 ---------------->");
+        PayrollDBService payrollDbService = new PayrollDBService();
+        employeePayrollService.updateSalary(payrollDbService.getConnection());
 
     }
-
 
 }
